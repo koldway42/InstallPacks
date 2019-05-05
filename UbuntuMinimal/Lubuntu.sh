8d134b8219923e957c434315b002f54c5f053d5b
Koldway42 <math12125.jr@gmail.com>
2019-05-04 21:07:31 -0300

    Addição das configurações para lubuntu e xubuntu

---
+++ UbuntuMinimal/Lubuntu.sh
@@ -0,0 +1,15 @@
+echo Instalando Pacotes...
+sudo apt-get install lubuntu-core^ gpm mc htop curl wget screenfetch
+echo configurando gpm
+systemctl enable gpm
+systemctl start gpm
+sudo apt-get update
+echo instalando NODE JS...
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt-get install -y nodejs mysql-server php5-common libapache2-mod-php5 php5-cli apache2
+sudo ufw allow 'Apache'
+node --version
+npm --version
+npm --global nodemon
+systemctl enable httpd
+systemctl start httpd
